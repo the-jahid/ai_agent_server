fa763ae9286cfb0abc19263ed21be6414ffb2817
the-jahid <aenor007@gmail.com>
2024-06-21 17:11:43 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+// index.js
+import express from 'express';
+import twilio from 'twilio';
+import axios from 'axios';
+import bodyParser from "body-parser";
+
+const { twiml: { VoiceResponse } } = twilio;
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+async function query(data) {
+    const response = await axios.post(
+        "https://aigeenee.chromovision.com/api/v1/prediction/88407ef1-b7b0-4431-b5c5-24df8652e5a1",
+        data,
+        {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+    );
+    return response.data;
+}
+
+app.post('/voice', async (request, response) => {
+  console.log('req');
+  console.log(request.body)
+  const twiml = new VoiceResponse();
+  console.log('userSpeech', request.body.SpeechResult)
+  if (request.body.SpeechResult) {
+    console.log('speechFound');
+    try {
+      const aiResponse = await query({ question: request.body.SpeechResult });
+      console.log('airesponse', aiResponse);
+      twiml.say(aiResponse.text);
+    } catch (error) {
+      console.error('Error querying AI Agent:', error);
+      twiml.say('Sorry, there was an error processing your request.');
+    }
+  } else {
+    console.log('i am called');
+    const gather = twiml.gather({
+      input: 'speech',
+      timeout: 5,
+      speechTimeout: 'auto',
+      action: '/voice'
+    });
+    gather.say('Hello my name is absher, How may I help you?');
+    twiml.redirect('/voice');
+  }
+
+  response.type('text/xml');
+  response.send(twiml.toString());
+ 
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
