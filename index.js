b308daf61deac7bcc1ef80fd8017d126563d185c
the-jahid <aenor007@gmail.com>
2024-06-23 14:38:23 +0600

    cors_updated

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ import express from 'express';
 import twilio from 'twilio';
 import axios from 'axios';
 import bodyParser from "body-parser";
+import cors from "cors";
 
 const { twiml: { VoiceResponse } } = twilio;
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
+
+
 async function query(data) {
     const response = await axios.post(
         "https://aigeenee.chromovision.com/api/v1/prediction/88407ef1-b7b0-4431-b5c5-24df8652e5a1",
@@ -26,13 +30,14 @@ app.post('/voice', async (request, response) => {
   console.log('req');
   console.log(request.body)
   const twiml = new VoiceResponse();
+  
   console.log('userSpeech', request.body.SpeechResult)
   if (request.body.SpeechResult) {
     console.log('speechFound');
     try {
       const aiResponse = await query({ question: request.body.SpeechResult });
       console.log('airesponse', aiResponse);
-      twiml.say(aiResponse.text);
+      twiml.say({voice:'Polly.Arlet-Neural'},aiResponse.text);
     } catch (error) {
       console.error('Error querying AI Agent:', error);
       twiml.say('Sorry, there was an error processing your request.');
@@ -45,7 +50,7 @@ app.post('/voice', async (request, response) => {
       speechTimeout: 'auto',
       action: '/voice'
     });
-    gather.say('Hello my name is absher, How may I help you?');
+    gather.say({voice:'Polly.Arlet-Neural'},'Hello my name is absher, How may I help you?');
     twiml.redirect('/voice');
   }
 
